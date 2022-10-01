68790559a07721d5395042403ed37eb60a45d01b
Nishant Sapkota <snishant306@gmail.com>
2022-10-01 13:55:53 +0545

    chore: prettier formatting

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-## Omegle Clone 
-This is an simple clone of an sussy chatting site  "Omegle".  
+## Omegle Clone
+
+This is an simple clone of an sussy chatting site "Omegle".
--- public/script.js
+++ public/script.js
@@ -1,184 +1,177 @@
-const socket = io('/');
+const socket = io("/");
 
-const conversation = document.querySelector('.conversation');
+const conversation = document.querySelector(".conversation");
 
 let alreadyTyping = false;
 
-socket.on('numberOfOnline', size => {
-    document.querySelector('.online').innerHTML = `${size.toLocaleString()} online now`
-}); 
-
-document.querySelector('#start').addEventListener('click', () => {
- 
-    socket.emit('start', socket.id);
+socket.on("numberOfOnline", (size) => {
+  document.querySelector(
+    ".online"
+  ).innerHTML = `${size.toLocaleString()} online now`;
 });
 
-socket.on('searching', msg => {
-    conversation.innerHTML = `<div class="message">${msg}</div>`;
+document.querySelector("#start").addEventListener("click", () => {
+  socket.emit("start", socket.id);
 });
 
-socket.on('chatStart', msg => {
-
-    conversation.innerHTML = `<div class="message">${msg}</div>`;
-
-    document.querySelector('#stop').classList.remove('hide');
-
-    document.querySelector('#start').classList.add('hide');
-
-    document.querySelector('#text').disabled = false;
-
-    document.querySelector('#send').disabled = false;
+socket.on("searching", (msg) => {
+  conversation.innerHTML = `<div class="message">${msg}</div>`;
 });
 
-document.querySelector('.form').addEventListener('submit', e => {
+socket.on("chatStart", (msg) => {
+  conversation.innerHTML = `<div class="message">${msg}</div>`;
+
+  document.querySelector("#stop").classList.remove("hide");
+
+  document.querySelector("#start").classList.add("hide");
+
+  document.querySelector("#text").disabled = false;
+
+  document.querySelector("#send").disabled = false;
+});
+
+document.querySelector(".form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  submitMessage();
+});
+
+document.querySelector("#text").onkeydown = (e) => {
+  if (e.keyCode === 13 && !e.shiftKey) {
     e.preventDefault();
     submitMessage();
-});
+  }
+};
 
+document.querySelector("#text").addEventListener("input", (e) => {
+  if (!alreadyTyping) {
+    socket.emit("typing", "Stranger is typing...");
 
-document.querySelector('#text').onkeydown = e => {
+    alreadyTyping = true;
+  }
 
-    if(e.keyCode === 13 && !e.shiftKey) {
-        e.preventDefault();
-        submitMessage();
-    }
-}
-
-document.querySelector('#text').addEventListener('input', e => {
-    if(!alreadyTyping) {
-
-        socket.emit('typing', 'Stranger is typing...');
-
-        alreadyTyping = true;
-    }
-
-    if(e.target.value === '') {
-        socket.emit('doneTyping');
-
-        alreadyTyping = false;
-    }
-});
-
-document.querySelector('#text').addEventListener('blur', () => {
-    socket.emit('doneTyping');
+  if (e.target.value === "") {
+    socket.emit("doneTyping");
 
     alreadyTyping = false;
+  }
 });
 
+document.querySelector("#text").addEventListener("blur", () => {
+  socket.emit("doneTyping");
 
-document.querySelector('#text').addEventListener('click', e => {
-    if(e.target.value !== '') {
-        socket.emit('typing', 'Stranger is typing...');
-
-        alreadyTyping = true;
-    }
+  alreadyTyping = false;
 });
 
-socket.on('newMessageToClient', data => {
-    const notStranger = data.id === socket.id;
-    const msg = htmlEncode(data.msg);
+document.querySelector("#text").addEventListener("click", (e) => {
+  if (e.target.value !== "") {
+    socket.emit("typing", "Stranger is typing...");
 
-    conversation.innerHTML += `
+    alreadyTyping = true;
+  }
+});
+
+socket.on("newMessageToClient", (data) => {
+  const notStranger = data.id === socket.id;
+  const msg = htmlEncode(data.msg);
+
+  conversation.innerHTML += `
         <div class="chat">
-            <span class="${notStranger ? 'name blue' : 'name red'}">${notStranger ? 'You: ' : 'Stranger: '} </span>
+            <span class="${notStranger ? "name blue" : "name red"}">${
+    notStranger ? "You: " : "Stranger: "
+  } </span>
             <span class="text">${msg}</span>
         </div>
     `;
 
-    conversation.scrollTo(0, conversation.scrollHeight);
+  conversation.scrollTo(0, conversation.scrollHeight);
 });
 
+socket.on("strangerIsTyping", (msg) => {
+  conversation.innerHTML +=
+    conversation.innerHTML = `<div class="message typing">${msg}</div>`;
 
-socket.on('strangerIsTyping', msg => {
-    conversation.innerHTML += conversation.innerHTML = `<div class="message typing">${msg}</div>`;
-
-    conversation.scrollTo(0, conversation.scrollHeight);
+  conversation.scrollTo(0, conversation.scrollHeight);
 });
 
-socket.on('strangerIsDoneTyping', () => {
-    const typing = document.querySelector('.typing');
+socket.on("strangerIsDoneTyping", () => {
+  const typing = document.querySelector(".typing");
 
-    if(typing) {
-        typing.remove();
-    }
+  if (typing) {
+    typing.remove();
+  }
 });
 
-socket.on('goodBye', msg => {
-    conversation.innerHTML += `<div class="message">${msg}</div>`;
+socket.on("goodBye", (msg) => {
+  conversation.innerHTML += `<div class="message">${msg}</div>`;
 
-    reset();
+  reset();
 });
 
-document.querySelector('#stop').addEventListener('click', () => {
-    document.querySelector('#stop').classList.add('hide');
+document.querySelector("#stop").addEventListener("click", () => {
+  document.querySelector("#stop").classList.add("hide");
 
-    document.querySelector('#really').classList.remove('hide');
+  document.querySelector("#really").classList.remove("hide");
 });
 
-document.querySelector('#really').addEventListener('click', () => {
-    // stop conversation
-    socket.emit('stop');
+document.querySelector("#really").addEventListener("click", () => {
+  // stop conversation
+  socket.emit("stop");
 });
 
-socket.on('strangerDisconnected', msg => {
-    conversation.innerHTML += `<div class="message">${msg}</div>`;
+socket.on("strangerDisconnected", (msg) => {
+  conversation.innerHTML += `<div class="message">${msg}</div>`;
 
-    reset();
+  reset();
 });
 
-socket.on('endChat', msg => {
-    conversation.innerHTML += `<div class="message">${msg}</div>`;
+socket.on("endChat", (msg) => {
+  conversation.innerHTML += `<div class="message">${msg}</div>`;
 
-    reset();
+  reset();
 });
 
 function submitMessage() {
-    const input = document.querySelector('#text');
+  const input = document.querySelector("#text");
 
-    if(/\S/.test(input.value)) {
-        socket.emit('doneTyping');
+  if (/\S/.test(input.value)) {
+    socket.emit("doneTyping");
 
-        socket.emit('newMessageToServer', input.value);
+    socket.emit("newMessageToServer", input.value);
 
-        input.value = '';
+    input.value = "";
 
-        alreadyTyping = false;
-    }
+    alreadyTyping = false;
+  }
 }
 
 function reset() {
-    
-    document.querySelector('#start').classList.remove('hide');
+  document.querySelector("#start").classList.remove("hide");
 
+  document.querySelector("#stop").classList.add("hide");
 
-    document.querySelector('#stop').classList.add('hide');
+  document.querySelector("#really").classList.add("hide");
 
-    document.querySelector('#really').classList.add('hide');
+  const text = document.querySelector("#text");
 
-    const text = document.querySelector('#text');
+  text.disabled = true;
 
+  text.value = "";
 
-    text.disabled = true;
+  document.querySelector("#send").disabled = true;
 
-    text.value = '';
+  const typing = document.querySelector(".typing");
 
+  if (typing) {
+    typing.remove();
+  }
 
-    document.querySelector('#send').disabled = true;
+  alreadyTyping = false;
 
-
-    const typing = document.querySelector('.typing');
-
-    if(typing) {
-        typing.remove();
-    }
-
-    alreadyTyping = false;
-
-    conversation.scrollTo(0, conversation.scrollHeight);
+  conversation.scrollTo(0, conversation.scrollHeight);
 }
 
-function htmlEncode(str){
-    return String(str).replace(/[^\w. ]/gi, function(c){
-        return '&#'+c.charCodeAt(0)+';';
-    });
+function htmlEncode(str) {
+  return String(str).replace(/[^\w. ]/gi, function (c) {
+    return "&#" + c.charCodeAt(0) + ";";
+  });
 }
